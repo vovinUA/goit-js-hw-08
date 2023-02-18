3858d3e429755083f1c159ab6598324b150fc502
vovinUA <vovin_tool@yahoo.com>
2023-02-18 19:02:02 +0200

    JS HW 08 task 3 fix

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,5 +1,9 @@
 import throttle from 'lodash.throttle';
 
+const email = document.querySelector('.feedback-form input[name="email"]');
+const message = document.querySelector(
+  '.feedback-form textarea[name="message"]'
+);
 const form = document.querySelector('.feedback-form');
 form.addEventListener('input', throttle(onFormData, 500));
 form.addEventListener('submit', onSubmitForm);
@@ -12,20 +16,20 @@ function onFormData(event) {
 }
 
 function onSubmitForm(event) {
-  console.log(JSON.parse(localStorage.getItem('feedback-form-state')));
+  console.log(formData);
   event.preventDefault();
   event.currentTarget.reset();
   localStorage.removeItem('feedback-form-state');
   formData = {};
 }
 
-(function dataFromLocalStorage() {
-  const data = JSON.parse(localStorage.getItem('feedback-form-state'));
-  const email = document.querySelector('.feedback-form input[name="email"]');
-  const message = document.querySelector('.feedback-form textarea[name="message"]');
-  if (data) {
-    formData = data;
-    email.value = formData.email || '';
-    message.value = formData.message || '';
+if (localStorage.getItem('feedback-form-state')) {
+  try {
+    formData = JSON.parse(localStorage.getItem('feedback-form-state'));
+  } catch (error) {
+    console.log(error.name);
+    console.log(error.message);
   }
-})();
+  email.value = formData.email || '';
+  message.value = formData.message || '';
+}
